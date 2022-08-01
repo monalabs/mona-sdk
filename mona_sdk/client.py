eedfff2f9063532ecff025b1c26c858808e7c9cd
Smadar <smadar@monalabs.io>
2022-08-01 11:07:44 +0300

    reformatted

--- mona_sdk/client.py
+++ mona_sdk/client.py
@@ -234,7 +234,9 @@ class Client:
             override_host=override_app_server_host
         )
         self.filter_none_fields_on_export = filter_none_fields_on_export
-        self._context_class_to_sampling_rate = (context_class_to_sampling_rate if context_class_to_sampling_rate else {})
+        self._context_class_to_sampling_rate = (
+            context_class_to_sampling_rate if context_class_to_sampling_rate else {}
+        )
 
     def _get_rest_api_export_url(self, override_host=None):
         http_protocol = "https" if self.should_use_ssl else "http"
@@ -339,7 +341,9 @@ class Client:
 
     def _should_sample_message(self, message):
         context_class = message.get(CONTEXT_CLASS_FIELD_NAME)
-        context_class_sampling_rate = self._context_class_to_sampling_rate.get(context_class)
+        context_class_sampling_rate = self._context_class_to_sampling_rate.get(
+            context_class
+        )
         if context_class_sampling_rate:
             context_id = message.get(CONTEXT_ID_FIELD_NAME)
             context_id_hash_value = calculate_normalized_hash(context_id)
