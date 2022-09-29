01687ecf04ca3cb4870042b2854302f92247afe4
Smadar <smadar@monalabs.io>
2022-09-29 17:12:20 +0300

    simplified more

--- mona_sdk/async_client.py
+++ mona_sdk/async_client.py
@@ -26,13 +26,8 @@ def async_wrap(func):
 
 
 class AsyncMeta(type):
-    def __new__(
-        metacls, name, bases, namespace, client_loop=None, client_executor=None
-    ):
-        return super().__new__(metacls, name, bases, namespace)
-
     def __init__(
-        metacls, class_name, bases, class_dict, client_loop=None, client_executor=None
+        metacls, class_name, bases, class_dict
     ):
         for attr_name in dir(metacls):
             if attr_name.startswith("__") or attr_name.startswith("_"):
@@ -40,21 +35,18 @@ class AsyncMeta(type):
 
             current_method = getattr(metacls, attr_name)
             if hasattr(current_method, "__call__"):
-                current_method_as_asynch = async_wrap(
+                current_method_as_async = async_wrap(
                     current_method
                 )
-                setattr(metacls, f"{attr_name}_asynch", current_method_as_asynch)
+                setattr(metacls, f"{attr_name}_async", current_method_as_async)
         # not need for the `return` here
         super(AsyncMeta, metacls).__init__(class_name, bases, class_dict)
 
 
-def get_async_client(*args, event_loop=None, executor=None, **kwargs):
-    class AsyncClient(
-        Client, metaclass=AsyncMeta, client_loop=event_loop, client_executor=executor
-    ):
-        def __init__(self, args, kwargs):
-            super().__init__(*args, **kwargs)
-            self._event_loop = event_loop
-            self._executor = executor
-
-    return AsyncClient(args, kwargs)
+class AsyncClient(
+    Client, metaclass=AsyncMeta
+):
+    def __init__(self, *args, event_loop=None, executor=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._event_loop = event_loop
+        self._executor = executor
