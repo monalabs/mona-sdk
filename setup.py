3bf94c0f70d40ca7a03c46f15b16fa4f2c0df5a0
Smadar <smadar@monalabs.io>
2023-07-16 14:50:27 +0300

    v++

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mona_sdk",
-    version="0.0.49",
+    version="0.0.50",
     author="MonaLabs",
     author_email="sdk@monalabs.io",
     description="SDK for communicating with Mona's servers",
