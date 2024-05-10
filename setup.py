cbbfc5ade3669943a662b7443c24a0f399d65b17
Nemo Tamir <nemo@monalabs.io>
2024-05-11 01:51:06 +0300

    version 54

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mona_sdk",
-    version="0.0.53",
+    version="0.0.54",
     author="MonaLabs",
     author_email="sdk@monalabs.io",
     description="SDK for communicating with Mona's servers",
