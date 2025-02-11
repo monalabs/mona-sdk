30ea00e5aff5865866b4b31c671149b2ef09f545
Elie Kotlarsky <elie@monalabs.io>
2025-02-11 14:40:21 +0200

    Fix import

--- mona_sdk/__init__.py
+++ mona_sdk/__init__.py
@@ -1,4 +1,5 @@
 """The Mona python client package"""
-from mona_sdk.client import Client, MonaSingleMessage
+from mona_sdk.client import Client
+from mona_sdk.mona_single_message import MonaSingleMessage
 
 name = "mona_sdk"
